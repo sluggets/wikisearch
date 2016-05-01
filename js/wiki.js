d74e50fde1282d398ba25289537bf7900ef578b0
timothy <tim.longoria@gmail.com>
2016-04-30 20:33:36 -0500

    Handles book clicking, and search box appearing
    Adds scaling to hover on Guidebook picture. Adds jquery UI for
    fade effect. Adds fade in for search box upon clicking Guidebook.

--- js/wiki.js
+++ js/wiki.js
@@ -1,16 +1,9 @@
 $(document).ready(function() {
-  window.onload = function () {
-    var canvas = document.getElementById("book");
-    var ctx = canvas.getContext("2d");
-    var img = document.createElement("img");
-    img.src = "/home/timothy/Dropbox/wikisearch/img/hitchpng.png";
-    img.addEventListener("load", function () {
-      ctx.drawImage(img, 0, 0);
-    }, false);
 
-  };
-
-  //document.getElementById("book").
+  $("#guide").click(function() {
+    $(".search").toggle("fade");
+  });
   
 });
+
 
